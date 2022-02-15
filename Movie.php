303a33dd979e69892fd2f08192ddcfe92810d92c
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-02-15 16:08:37 +0100

    add matrix

--- Movie.php
+++ Movie.php
@@ -2,9 +2,21 @@
 
     class Movie {
 
-        protected $title;
-
-
+        public $title;
+        public $actors;
+        public $genre; 
+        public $movie_director;
+        public $plot; 
+        public $year; 
     }
 
+
+
+
+    $matrix = new Movie ('');
+    $matrix -> title = "Matrix";
+    var_dump ($matrix)
+
+    
 ?>
+
